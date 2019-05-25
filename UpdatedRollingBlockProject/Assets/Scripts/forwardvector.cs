47172304db04ab31990a1a9ca65e0b84b9da0a06
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-05-25 11:30:51 -0700

    added forwardvector and cam

---
+++ UpdatedRollingBlockProject/Assets/Scripts/forwardvector.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System;
+
+public class forwardvector : MonoBehaviour
+{
+    public Vector3 forward;
+    private double sin = Math.Sin(.5);
+    private double cos = Math.Cos(.5);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKey("q"))
+        {
+            float nx = (float)(forward.x * cos - forward.z * sin);
+            float nz = (float)(forward.x * sin + forward.z * cos);
+            float ny = (float)forward.y;
+            forward.Set(nx, ny, nz);
+        }
+    }
+}

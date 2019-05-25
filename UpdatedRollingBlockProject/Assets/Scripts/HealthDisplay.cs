bbf78fdbbbc6fa14451d23edc9bccaa390b6a48b
Keaton Drebes <keatondrebes@Keatons-MBP.wv.cc.cmu.edu>
2019-05-25 15:58:59 -0400

    UI?

--- UpdatedRollingBlockProject/Assets/Scripts/HealthDisplay.cs
+++ UpdatedRollingBlockProject/Assets/Scripts/HealthDisplay.cs
@@ -11,12 +11,6 @@ public class HealthDisplay : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        scoreText = "ScoreText: " + score;
     }
 }

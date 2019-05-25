82ed330192e1517a2eb6bec426ae8b2c500f4fe8
Keaton Drebes <keatondrebes@Keatons-MBP.wv.cc.cmu.edu>
2019-05-25 13:31:20 -0400

    blegh

---
+++ UpdatedRollingBlockProject/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody ThePlayer;
+    public float Speed;
+    Vector3 forceVector;
+    forceVector = Vector3(0f,0f, Speed);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ThePlayer.AddForce()
+
+    }
+}

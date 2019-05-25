f8376d0709aa42ef4868a048fef84d714ea06b65
AlexanderLuasan <38295434+AlexanderLuasan@users.noreply.github.com>
2019-05-25 12:56:18 -0700

    new hit detection script

---
+++ UpdatedRollingBlockProject/Assets/Scripts/collisionWithGround.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class collisionWithGround : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "ground")
+        {
+            Debug.Log("hit Ground");
+        }
+
+
+    }
+}

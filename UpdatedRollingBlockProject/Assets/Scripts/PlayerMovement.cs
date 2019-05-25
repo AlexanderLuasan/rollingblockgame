b3f46ac344b8ffd44ebd6bdae84e7c545408d1a6
Keaton Drebes <keatondrebes@Keatons-MBP.wv.cc.cmu.edu>
2019-05-25 13:37:04 -0400

    fixed script?

--- UpdatedRollingBlockProject/Assets/Scripts/PlayerMovement.cs
+++ UpdatedRollingBlockProject/Assets/Scripts/PlayerMovement.cs
@@ -6,18 +6,23 @@ public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody ThePlayer;
     public float Speed;
+    public string moveButton;
     Vector3 forceVector;
-    forceVector = Vector3(0f,0f, Speed);
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        forceVector = new Vector3(0f, 0f, Speed);
     }
 
     // Update is called once per frame
     void Update()
     {
-        ThePlayer.AddForce()
+        if (Input.GetButtonDown(moveButton))
+        {
+            ThePlayer.AddForce(forceVector)
+        }
 
     }
 }
